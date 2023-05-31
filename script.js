2850f393d8aed81500859451c4b5bc68b20a8fbb
BrenoBar51 <brenobarcelos9@gmail.com>
2023-05-31 09:59:40 -0300

    script terminado

---
+++ script.js
@@ -0,0 +1,18 @@
+const linkDropdown = document.getElementById('more')
+const locationDropdown = document.querySelector('#location')
+const cardDropdown = document.querySelector('#link-dropdown')
+function openLinkDropdown(){
+    cardDropdown.style.display = 'flex'
+}
+function closeLinkDropdown(){
+    cardDropdown.style.display = 'none'
+}
+function openLocationDropdown(){
+    console.log('abriu')
+}
+
+linkDropdown.addEventListener('mouseover', openLinkDropdown)
+linkDropdown.addEventListener('mouseout', closeLinkDropdown)
+cardDropdown.addEventListener('mouseover', openLinkDropdown)
+cardDropdown.addEventListener('mouseout', closeLinkDropdown)
+locationDropdown.addEventListener('mouseover', openLocationDropdown)
